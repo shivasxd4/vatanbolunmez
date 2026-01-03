53683c1e3258e4a7ed294fdc8e1aa3a68d9f74c8
shivasxd4 <153465408+shivasxd4@users.noreply.github.com>
2026-01-03 05:28:02 +0300

    Update server.js

--- server.js
+++ server.js
@@ -9,42 +9,37 @@ const io = socketIo(server, { cors: { origin: "*" } });
 
 app.use(express.static(__dirname));
 
-const rooms = {};
+let waitingUsers = [];
 
 io.on('connection', (socket) => {
-    socket.on('join-room', ({ roomId, nickname, avatar }) => {
-        socket.join(roomId);
-        if (!rooms[roomId]) rooms[roomId] = [];
-        rooms[roomId].push({ id: socket.id, nickname, avatar });
-        
-        const otherUsers = rooms[roomId].filter(u => u.id !== socket.id).map(u => u.id);
-        socket.emit('all-users', otherUsers);
-        io.to(roomId).emit('room-update', rooms[roomId]);
+    socket.on('join-random', (userData) => {
+        socket.userData = userData;
+        if (waitingUsers.length > 0) {
+            const partner = waitingUsers.shift();
+            const roomId = `room_${socket.id}_${partner.id}`;
+            socket.join(roomId);
+            partner.join(roomId);
+            socket.emit('matched', { roomId, partner: partner.userData, partnerId: partner.id, initiator: true });
+            partner.emit('matched', { roomId, partner: socket.userData, partnerId: socket.id, initiator: false });
+        } else {
+            waitingUsers.push(socket);
+            socket.emit('waiting');
+        }
     });
 
-    socket.on('offer', payload => {
-        io.to(payload.userToSignal).emit('offer', { sdp: payload.sdp, callerID: payload.callerID });
+    socket.on('signal', data => {
+        io.to(data.to).emit('signal', { from: socket.id, signal: data.signal });
     });
 
-    socket.on('answer', payload => {
-        io.to(payload.callerID).emit('answer', { sdp: payload.sdp, id: socket.id });
-    });
-
-    socket.on('ice-candidate', payload => {
-        io.to(payload.target).emit('ice-candidate', { candidate: payload.candidate, from: socket.id });
-    });
-
-    socket.on('send-chat', (data) => {
+    socket.on('send-chat', data => {
         io.to(data.roomId).emit('receive-chat', data);
     });
 
     socket.on('disconnect', () => {
-        for (const rId in rooms) {
-            rooms[rId] = rooms[rId].filter(u => u.id !== socket.id);
-            io.to(rId).emit('room-update', rooms[rId]);
-        }
+        waitingUsers = waitingUsers.filter(u => u.id !== socket.id);
+        io.emit('partner-disconnected');
     });
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log(`VK Royals running on ${PORT}`));
+server.listen(PORT, () => console.log(`Server running on ${PORT}`));
